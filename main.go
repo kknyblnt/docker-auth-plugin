b5b00d7ecd397c60cf162a1f16aff0a4de84c8cd
Balint Kakonyi <kakonyib14@gmail.com>
2024-04-14 19:49:59 +0200

    wip

--- main.go
+++ main.go
@@ -2,34 +2,13 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"os"
 )
 
 func kcmStart(filepath string) {
 
 	// Load Keycloak configuration from file
-	kcmConfigRaw := core.loadConfig(filepath)
-
-	// Parse kcm config
-	kcmConfig = core.parseKCMConfig(kcmConfigRaw)
-
-	// Retrieve access token
-	token, err := kcmConfig.GetAccessToken()
-	if err != nil {
-		log.Fatalf("Error retrieving access token: %v", err)
-	}
-
-	// Print the access token details
-	fmt.Println("Access Token:", token.AccessToken)
-	fmt.Println("Token Type:", token.TokenType)
-	fmt.Println("Expires In:", token.ExpiresIn)
-	fmt.Println("Refresh Token:", token.RefreshToken)
-	fmt.Println("Refresh Expires In:", token.RefreshExpiresIn)
-	fmt.Println("Not Before Policy:", token.NotBeforePolicy)
-	fmt.Println("Session State:", token.SessionState)
-	fmt.Println("Scope:", token.Scope)
-
+	configData, err := loadConfig("hello.json")
 }
 
 func main() {

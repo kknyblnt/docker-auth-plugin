e9685ac50048caa56bdbfaa5368c4023a367639e
Balint Kakonyi <kakonyib14@gmail.com>
2024-04-16 21:33:46 +0200

    unused function removed

--- plugin/handlers.go
+++ plugin/handlers.go
@@ -2,7 +2,6 @@ package plugin
 
 import (
 	kcm "docker-auth-plugin/auth/kc"
-	pluginconfig "docker-auth-plugin/core/config"
 	"fmt"
 	"log"
 )
@@ -32,45 +31,4 @@ func kcmHandleLogout(keycloakConfig *kcm.KeycloakConfig, refreshToken string) {
 	} else {
 		fmt.Println("Successfully logged out.")
 	}
-}
-
-func kcmStart(filepath string, username string, password string) {
-
-	// Load Keycloak configuration from file
-	configData, err := pluginconfig.LoadConfig("config.json")
-	if err != nil {
-		log.Fatalf("Error loading config: %v", err)
-	}
-	keycloakConfig, err := pluginconfig.ParseKCMConfig(configData)
-	if err != nil {
-		log.Fatalf("Error parsing Keycloak config: %v", err)
-	}
-
-	//GET ACCESS TOKEN
-	tokenResponse, err := kcmHandleGetAccessToken(*keycloakConfig, *kcm.NewKeycloakCredentials(username, password))
-	if err != nil {
-		log.Fatalf("Error getting access token: %v", err)
-	}
-
-	fmt.Println("ACCESS TOKEN RESPONSE:")
-	// Correctly print the access token
-	fmt.Println("Access Token:", tokenResponse.AccessToken)
-	fmt.Println("Token Type:", tokenResponse.TokenType)
-	fmt.Println("Expires In:", tokenResponse.ExpiresIn)
-	fmt.Println("Refresh Token:", tokenResponse.RefreshToken)
-	fmt.Println("Refresh Expires In:", tokenResponse.RefreshExpiresIn)
-	fmt.Println("Not Before Policy:", tokenResponse.NotBeforePolicy)
-	fmt.Println("Session State:", tokenResponse.SessionState)
-	fmt.Println("Scope:", tokenResponse.Scope)
-
-	//INTROSPECT
-	introspecResponse, err := kcmHandleTokenIntrospect(*keycloakConfig, tokenResponse)
-	if err != nil {
-		log.Fatalf("Error introspecting access token: %v", err)
-	}
-
-	fmt.Println("Active??? ", introspecResponse.Active)
-
-	kcmHandleLogout(keycloakConfig, tokenResponse.RefreshToken)
-
 }
